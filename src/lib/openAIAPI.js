f3f87e275b6f22ac6910ca249894fb56b8156b78
Alheli De Vicente <devicentealheli@gmail.com>
2024-08-06 17:39:24 -0600

    groupChat seems to be working

--- src/lib/openAIAPI.js
+++ src/lib/openAIAPI.js
@@ -1,6 +1,7 @@
 import { getApiKey } from "./apikey.js";
-export async function communicateWithOpenAi(message) {
-  try{
+
+export async function communicateWithOpenAi(userInput, selectedPhilosopher) {
+  try {
     const apiKey = getApiKey();
     const response = await fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
@@ -9,21 +10,27 @@ export async function communicateWithOpenAi(message) {
         "Authorization": `Bearer ${apiKey}`
       },
       body: JSON.stringify({
-        "model": "gpt-4o",
-        "messages": message
+        model: "gpt-4",
+        messages: [
+          {
+            role: "system",
+            content: `Eres el filósofo ${selectedPhilosopher.name}. Debes responder como él. Cuando te pregunten, sé bastante explícito en tus respuestas, que deben ser entre 50 y 100 palabras. Si puedes, recomienda bibliografía tuya si es posible. Adopta la personalidad del filósofo, aquí hay más información sobre ti: ${selectedPhilosopher.description}`
+          },
+          {
+            role: "user",
+            content: userInput
+          }
+        ]
       })
     });
-
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-
+    
     const data = await response.json();
     return data;
 
   } catch (error) {
     console.error("Error:", error.message);
-    throw error; 
+    throw error;
   }
 }
+
 
